348576c4f25c810dbe579770cfc552e3a75061c1
vianra <ravnsy@gmail.com>
2024-07-02 14:36:32 +0700

    Initial Commit

---
+++ src/main/java/id/ac/ui/cs/advprog/examplepegawai/ExamplepegawaiApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.examplepegawai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplepegawaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplepegawaiApplication.class, args);
+	}
+
+}
